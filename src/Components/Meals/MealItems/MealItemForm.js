15ce17e3a2e46f8ebfa8d483123d1ae17331118e
bhanu <udaibhanu2602@gmail.com>
2022-10-02 00:22:42 +0530

    Adding Form and Input Component Task-4

---
+++ src/Components/Meals/MealItems/MealItemForm.js
@@ -0,0 +1,26 @@
+import React from "react";
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
+const MealItemForm = () => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: 1,
+          max: 5,
+          defaultValue: 1,
+          step: 1,
+        }}
+      />
+      <button>+ADD</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
+
+
+//here in <Input>, in input={{}}, we are passing object i.e {{key:value}}}
--- src/Components/Meals/MealItems/MealItems.js
+++ src/Components/Meals/MealItems/MealItems.js
@@ -1,15 +1,17 @@
 import React from "react";
-import classes from './MealItems.module.css'
-
+import classes from "./MealItems.module.css";
+import MealItemForm from "./MealItemForm";
 const MealItems = (prop) => {
-
-const price=`$${prop.meal.price}`    
+  const price = `$${prop.meal.price}`;
   return (
     <li className={classes.meal}>
-      <div >
+      <div>
         <h3>{prop.meal.name}</h3>
-        <div  className={classes.description}>{prop.meal.description}</div>
+        <div className={classes.description}>{prop.meal.description}</div>
         <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm />
       </div>
     </li>
   );
